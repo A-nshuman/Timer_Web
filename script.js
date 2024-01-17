cbf5385933e35e7871dc8174196f1e7857b38a6d
Anshuman Bhardwaj <64587347+A-nshuman@users.noreply.github.com>
2024-01-18 00:01:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const seconds = document.getElementById('seconds');
+    const display_text = document.querySelector('.count');
+    const startBtn = document.getElementById('startBtn');
+    const hand = document.querySelector('.hand');
+    display_text.innerHTML = seconds.value
+
+    function timer() {
+        display_text.innerHTML = seconds.value;
+        seconds.value--;
+        hand.style.animation = `handAnim ${parseInt(seconds.value) + 1}s linear`
+        const timer_start = setInterval(() => {
+            if (seconds.value > 0) {
+                display_text.innerHTML = seconds.value;
+                console.log(seconds.value)
+                seconds.value--;
+                // seconds.style.color = 'white'
+            } else {
+                clearInterval(timer_start)
+                seconds.value = ''
+                seconds.style.color = 'black'
+                display_text.innerHTML = 'Timer Over'
+                hand.style.animation = `none`
+            }
+        }, 1000);
+    }
+
+    startBtn.addEventListener('click', () => {
+        timer()
+    })
+
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            timer()
+        }
+    })
+});
